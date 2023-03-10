36eace7089e218e198cf7e032950b3875fff0152
Roman Peklov <la2elstan@gmail.com>
2023-03-10 17:02:52 +0400

    v0.1.1 -small code fix

--- bottelega.py
+++ bottelega.py
@@ -3,20 +3,19 @@ import random
 import os
 import data
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 # import config #нужно для отладки оффлайн
 
 # bot = telebot.TeleBot(config.TOKEN)
 bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
-url = data.url
-html_text = requests.get(url)
+html_text = requests.get(data.url)
 
 # Function to receive a list of phrases to answer "who_am_i" question
 def get_phrase_list():
     phrase_list = []
-    soup = bs(html_text.text, 'html.parser')
+    soup = BeautifulSoup(html_text.text, 'html.parser')
     tags = soup.find_all("span")
     for tag in tags:
         text = tag.get_text()
@@ -24,21 +23,18 @@ def get_phrase_list():
     return phrase_list
 
 @bot.message_handler(commands=['start'])
-def booked(message):
+def start(message):
     start_message = data.start_message
     bot.send_message(message.chat.id, start_message)
 
 
-@bot.message_handler(commands=['booked'])
-def booked(message):
-    spb_first = data.spb_first
-
-    booked_info = f'Первая бронь в Питере: \n{spb_first},\n\n' \
-
-    bot.send_message(message.chat.id, booked_info)
+@bot.message_handler(commands=['info'])
+def info(message):
+    info = data.current_info
+    bot.send_message(message.chat.id, info)
 
 
-@bot.message_handler(['wifi'])
+@bot.message_handler(commands=['wifi'])
 def wifi(message):
     wifi_list = data.wifi_list
     bot.send_message(message.chat.id, wifi_list)
@@ -49,8 +45,6 @@ def who_am_i_sanya(message):
     if message.text == (data.message_text):
         whoami_name = get_phrase_list()[random.randint(0, len(get_phrase_list()) - 1)]
         bot.send_message(message.chat.id, f'Ты - {whoami_name}')
-    else:
-        pass
 
 
 if __name__ == "__main__":
--- data.py
+++ data.py
@@ -9,10 +9,9 @@ start_message = 'Здравствуйте, я Александр!'
 message_text = 'Сантей кто я'
 
 #
+current_info = 'No info today :('
+
+#
 wifi_list = 'Карелия:\n' \
             'Wi-Fi: Keenetic-2574\n' \
             'Пароль: xMwM2ohx\n'
-#
-spb_first = 'https://ostrovok.ru/hotel/russia/st._petersburg/mid8094163/apartments_on_yegorova/?q=2042&dates=' \
-            '12.06.2022-13.06.2022&guests=4&utm_source=mobile&utm_content=AB5FEE7C2939876273EB8EE9A8ADB5F0&' \
-            'utm_medium=sharing&utm_campaign=hotelpage&sid=4faf46dc-1a36-43ea-8355-a2b5421bffa7'
