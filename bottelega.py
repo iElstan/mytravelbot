b24d1efcd64a873a34425dce5fc317fcf6289983
Roman Peklov <la2elstan@gmail.com>
2023-03-10 16:23:13 +0400

    v0.1.0 - code refactoring

--- bottelega.py
+++ bottelega.py
@@ -2,68 +2,52 @@ import telebot
 import random
 import os
 import data
+import requests
+from bs4 import BeautifulSoup as bs
+
 # import config #нужно для отладки оффлайн
 
 # bot = telebot.TeleBot(config.TOKEN)
 bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
+url = data.url
+html_text = requests.get(url)
+
+# Function to receive a list of phrases to answer "who_am_i" question
+def get_phrase_list():
+    phrase_list = []
+    soup = bs(html_text.text, 'html.parser')
+    tags = soup.find_all("span")
+    for tag in tags:
+        text = tag.get_text()
+        phrase_list.append(text)
+    return phrase_list
 
 @bot.message_handler(commands=['start'])
 def booked(message):
-    start_message = 'Здравствуйте, я Александр!'
+    start_message = data.start_message
     bot.send_message(message.chat.id, start_message)
 
 
 @bot.message_handler(commands=['booked'])
 def booked(message):
-    spb_first = 'https://ostrovok.ru/hotel/russia/st._petersburg/mid8094163/apartments_on_yegorova/?q=2042&dates=' \
-                '12.06.2022-13.06.2022&guests=4&utm_source=mobile&utm_content=AB5FEE7C2939876273EB8EE9A8ADB5F0&' \
-                'utm_medium=sharing&utm_campaign=hotelpage&sid=4faf46dc-1a36-43ea-8355-a2b5421bffa7'
-
-    spb_main = 'Адрес: Санкт-Петербург, ул. Жуковского 5.\n ' \
-               '\n' \
-               'Заезд 13 июня в 15.00\n' \
-                'Выезд 15 июня в 12.00\n' \
-                '\n' \
-                'Телефон +7 910 762-82-81 \n' \
-               'Телефон +7 921 964-86-15 \n' \
-               'Оплата при заселении 11730р\n'
-
-    karel = 'Адрес: Республика Карелия, Лахденпохский р-он п.Микли, дальше по указателям 1-2 км до хутор Медовое.\n ' \
-            '\n' \
-            'GPS-координаты 61.520609, 30.331034.\n' \
-            '\n' \
-            'Сообщить  за 1 час о своём приезде\n' \
-            'Заезд в 17.00\n' \
-            'Выезд в 14.00\n' \
-            '\n' \
-            'По всем вопросам звонить по номеру +79216369300 Оксана\n' \
-            '\n' \
-            'При заселении берётся залог в размере 5000р\n' \
-            'Проживание питомца за доп.плату.\n' \
-            '\n' \
-            'Wi-Fi: Keenetic-2574\n' \
-            'Пароль: xMwM2ohx\n'
+    spb_first = data.spb_first
 
     booked_info = f'Первая бронь в Питере: \n{spb_first},\n\n' \
-                  f'Основная бронь в Питере: \n{spb_main},\n\n' \
-                  f'Бронь в Карелии: \n{karel}\n'
+
     bot.send_message(message.chat.id, booked_info)
 
 
 @bot.message_handler(['wifi'])
 def wifi(message):
-    wifi_list = 'Карелия:\n' \
-                'Wi-Fi: Keenetic-2574\n' \
-                'Пароль: xMwM2ohx\n'
+    wifi_list = data.wifi_list
     bot.send_message(message.chat.id, wifi_list)
 
 
 @bot.message_handler()
 def who_am_i_sanya(message):
-    # whoami_question = input()
-    if message.text == ('Сантей кто я'):
-        whoami_name = data.get_phrase_list()[random.randint(0, len(data.get_phrase_list()) - 1)]
+    if message.text == (data.message_text):
+        whoami_name = get_phrase_list()[random.randint(0, len(get_phrase_list()) - 1)]
         bot.send_message(message.chat.id, f'Ты - {whoami_name}')
     else:
         pass
--- data.py
+++ data.py
@@ -1,20 +1,18 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
+# URL from where the parset get phrases needed
 url = 'http://dict.ruslang.ru/magn.php?act=search&orderby=context&magn_lex=&context_lex=&magn_ps=&context_ps=' \
       '%F1%F3%F9%E5%F1%F2%E2%E8%F2%E5%EB%FC%ED%EE%E5&magn_sem='
-html_text = requests.get(url)
 
+#
+start_message = 'Здравствуйте, я Александр!'
 
-def get_phrase_list():
-    phrase_list = []
-    soup = bs(html_text.text, 'html.parser')
-    tags = soup.find_all("span")
-    for tag in tags:
-        text = tag.get_text()
-        phrase_list.append(text)
-    return phrase_list
+#
+message_text = 'Сантей кто я'
 
-
-if __name__ == '__main__':
-    get_phrase_list()
+#
+wifi_list = 'Карелия:\n' \
+            'Wi-Fi: Keenetic-2574\n' \
+            'Пароль: xMwM2ohx\n'
+#
+spb_first = 'https://ostrovok.ru/hotel/russia/st._petersburg/mid8094163/apartments_on_yegorova/?q=2042&dates=' \
+            '12.06.2022-13.06.2022&guests=4&utm_source=mobile&utm_content=AB5FEE7C2939876273EB8EE9A8ADB5F0&' \
+            'utm_medium=sharing&utm_campaign=hotelpage&sid=4faf46dc-1a36-43ea-8355-a2b5421bffa7'
